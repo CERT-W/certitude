0349f81c6cf8a29033c9fec8e7e8ffdd9341eb37
Jean MARSAULT <jean.marsault@solucom.fr>
2015-11-12 10:05:43 +0100

    Increased security in certificate generation (see issue #6) - Fixed key length to 4096 (sould last long enough) - Set signature algorithm to sha256WithRsaEncryption

--- ssl/gen-cert-for-me.bat
+++ ssl/gen-cert-for-me.bat
@@ -1,6 +1,10 @@
 @echo off
 
-openssl genrsa -f4 -out server.pem.key
-openssl req -new -key server.pem.key -out server.csr
-openssl x509 -req -signkey server.pem.key -in server.csr -days 365 -out server.pem.cer
-del server.csr
+set "KEY_FILE=server.pem.key"
+set "CSR_FILE=server.pem.csr"
+set "CER_FILE=server.pem.cer"
+
+openssl genrsa -f4 -out "%KEY_FILE%" 4096
+openssl req -new -key "%KEY_FILE%" -out "%CSR_FILE%" -sha256
+openssl x509 -req -signkey "%KEY_FILE%" -in "%CSR_FILE%" -days 365 -out "%CER_FILE%" -sha256
+del "%CSR_FILE%"
