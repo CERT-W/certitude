c0a533cd4d0bd29627048aea0009268d9edf49d6
Jean MARSAULT <jean.marsault@solucom.fr>
2016-02-18 15:36:13 +0100

    [hashscanner] Fixed typo in sql import

--- components/scanner/resources/sql/files_hash.sql
+++ components/scanner/resources/sql/files_hash.sql
@@ -5,7 +5,7 @@ CREATE TABLE files_hash
 	FilePath TEXT,					-- File full path
 	Md5Sum VARCHAR(32),					-- File MD5
 	Sha1Sum VARCHAR(40),					-- File SHA-1
-	Sha256Sum Varchar(64),					-- File SHA-256
+	Sha256Sum Varchar(64)					-- File SHA-256
 );
 
 .separator '	'
