024bdd930cd55265e626f0d1fcf68e73ff47af37
Jean MARSAULT <jean.marsault@solucom.fr>
2016-11-03 12:02:49 +0100

    Copyright update

--- hashscan.bat
+++ hashscan.bat
@@ -1,3 +1,24 @@
+REM CERTitude: the seeker of IOC
+REM Copyright (c) 2016 CERT-W
+
+REM Contact: cert@wavestone.com
+REM Contributors: @iansus, @nervous, @fschwebel
+
+REM CERTitude is under licence GPL-2.0:
+REM This program is free software; you can redistribute it and/or
+REM modify it under the terms of the GNU General Public License
+REM as published by the Free Software Foundation; either version 2
+REM of the License, or (at your option) any later version.
+
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+
+REM You should have received a copy of the GNU General Public License
+REM along with this program; if not, write to the Free Software
+REM Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 @echo off
 cls
 python main.py run -c hashscan
